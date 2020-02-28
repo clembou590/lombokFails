b6a03023bd3e87c8ee9d4413c8954904daedf870
clement.boret <clement.boret@tidal.com>
2020-02-28 16:11:14 +0100

    use of @delegate does not work

--- src/main/java/com/example/deleg/fail/MessageService.java
+++ src/main/java/com/example/deleg/fail/MessageService.java
@@ -3,14 +3,13 @@ package com.example.deleg.fail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lombok.experimental.Delegate;
+
 @Service
 public class MessageService {
 
     @Autowired
+    @Delegate
     private MessageRepository messageRepository;
-
-    public <S extends Message> S save(S entity) {
-        return messageRepository.save(entity);
-    }
 
 }
